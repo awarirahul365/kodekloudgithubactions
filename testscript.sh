1bc776b94ceec2c2f19cb74b49f3e300bc7606bb
Awari <rahul.awari@sap.com>
2024-09-15 15:27:05 +0530

    sec com

--- testscript.sh
+++ testscript.sh
@@ -1,2 +1,3 @@
 #/bin/sh
+ls 
 cat ascci.txt
--- testscript2.sh
+++ testscript2.sh
@@ -3,4 +3,3 @@ apt-get install sudo
 apt-get install cowsay -y
 sudo apt-get install cowsay -y
 cowsay -f dragon "Hello way" >> ascci.txt
-cat ascci.txt
