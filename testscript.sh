842ba86bb8a8fef18803ce99f51bb30c2d8fe9e1
Awari <rahul.awari@sap.com>
2024-09-15 15:15:38 +0530

    second job

---
+++ testscript.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+apt-get install sudo
+apt-get install cowsay -y
+sudo apt-get install cowsay -y
+cowsay -f dragon "Second jon" >> ascci.txt
+cat ascci.txt
---
+++ testscript2.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+apt-get install sudo
+apt-get install cowsay -y
+sudo apt-get install cowsay -y
+cowsay -f dragon "Hello way" >> ascci.txt
+cat ascci.txt
